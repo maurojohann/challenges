e05b3e2c65fc35719c34ebd41e9d7a213063ba01
Mauro Cezar Johann <mauro.johann@gmail.com>
2021-03-27 20:17:38 -0300

    feat/ total de letras por palavras

--- src/main/java/desafiosBootCampJava/AbreviandoPost.java
+++ src/main/java/desafiosBootCampJava/AbreviandoPost.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -19,6 +20,9 @@ public class AbreviandoPost {
             List<String> textPost = new ArrayList<>();
             List<Optional<String>> textabrev2 = new ArrayList<>();
             Map<String, List<String>> textAbrev = new HashMap<String, List<String>>();
+            Map<String, String> textAbrev3 = new HashMap<String, String>();
+            Map<String, Long> textAbrev4 = new HashMap<String, Long>();
+            Map<String, Long> textAbrev5 = new HashMap<String, Long>();
 
             while (st.hasMoreTokens()) {
                 textPost.add(st.nextToken());
@@ -29,16 +33,42 @@ public class AbreviandoPost {
             if (textPost.get(0).equals("."))
                 break;
 
-            textAbrev.putAll(textPost.stream().filter(s -> (s.length() > 2 && !s.matches(".")))
+            // textAbrev3.putAll(textPost.stream().filter(s -> (s.length() > 2 && !s.matches(".")))
+            //         .sorted((p1, p2) -> p1.compareTo(p2)).collect(Collectors.groupingByConcurrent(String::toString,
+            //                 ConcurrentSkipListMap::new, Collectors.joining("|"))));
+            textAbrev4.putAll(textPost.stream().filter(s -> (s.length() > 2 && !s.matches(".")))
+                    .sorted((p1, p2) -> p1.compareTo(p2)).collect(Collectors.groupingByConcurrent(String::toString,
+                         Collectors.counting())));
+
+                        textAbrev4.entrySet().stream().forEach(e ->  textAbrev5.put(e.getKey(), (e.getKey().length() * e.getValue())));
+
+            System.out.println("TextAbrev5 " + textAbrev5);
+
+            textAbrev.putAll(textAbrev3.values().stream().filter(s -> (s.length() > 2 && !s.matches(".")))
                     .collect(Collectors.groupingBy(o -> o.substring(0, 1))));
+
+            System.out.println("TextAbrev " + textAbrev);        
 
             textAbrev.forEach((s, strings) -> textabrev2
                     .add(strings.stream().reduce((s1, s2) -> s1.length() > s2.length() ? s1 : s2)));
+
+
+                    System.out.println("TextAbrev2 " + textabrev2);                    
+
             List<String> listWithoutNull = textabrev2.stream().flatMap(Optional::stream).collect(Collectors.toList());
+            List<String> listWithoutNull2 = listWithoutNull.stream()
+                    .map(mapper -> mapper.substring(0, mapper.indexOf("|"))).collect(Collectors.toList());
+
+
+                    System.out.println("listWithoutNull " + listWithoutNull);       
+
+                    System.out.println("listWithoutNull2 " + listWithoutNull2);                    
+                  
 
             AtomicInteger finalCountAbrev = new AtomicInteger();
+
             Optional<String> finals = textPost.stream().map(s -> {
-                if (listWithoutNull.contains(s)) {
+                if (listWithoutNull2.contains(s)) {
                     finalCountAbrev.getAndIncrement();
                     return s.substring(0, 1).concat(".");
                 } else {
@@ -50,7 +80,7 @@ public class AbreviandoPost {
             if (finalCountAbrev.intValue() >= 0) {
                 System.out.println(textAbrev.size());
             }
-            listWithoutNull.forEach(s -> System.out.println(s.substring(0, 1).concat(". = ").concat(s)));
+            listWithoutNull2.forEach(s -> System.out.println(s.substring(0, 1).concat(". = ").concat(s)));
 
         }
 
